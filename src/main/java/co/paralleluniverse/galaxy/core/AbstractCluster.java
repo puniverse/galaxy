f1dec52a697e037386a6b8b48b902e432759909b
eitan <eitan@paralleluniverse.co>
2013-06-06 15:17:07 +0300

    -

--- src/main/java/co/paralleluniverse/galaxy/core/AbstractCluster.java
+++ src/main/java/co/paralleluniverse/galaxy/core/AbstractCluster.java
@@ -231,6 +231,8 @@ public abstract class AbstractCluster extends Service implements Cluster {
             }
 
         });
+        
+        controlTree.print(ROOT, System.out);
 
         controlTree.addListener(LEADERS, new DistributedTree.ListenerAdapter() {
 
