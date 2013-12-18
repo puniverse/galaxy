e1ba7a0b33e1f74941839c23405a80d9cc599af3
eitan <eitan@paralleluniverse.co>
2013-12-18 18:32:35 +0200

    add Distributed interface

--- src/main/java/co/paralleluniverse/galaxy/objects/DistributedObject.java
+++ src/main/java/co/paralleluniverse/galaxy/objects/DistributedObject.java
@@ -1,41 +1,63 @@
-/*
- * Galaxy
- * Copyright (C) 2012 Parallel Universe Software Co.
- * 
- * This file is part of Galaxy.
- *
- * Galaxy is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of 
- * the License, or (at your option) any later version.
- *
- * Galaxy is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public 
- * License along with Galaxy. If not, see <http://www.gnu.org/licenses/>.
- */
 package co.paralleluniverse.galaxy.objects;
 
-import co.paralleluniverse.galaxy.CacheListener;
-import co.paralleluniverse.galaxy.Store;
+import co.paralleluniverse.common.io.Persistable;
 import java.nio.ByteBuffer;
 
 /**
+ * Wraps T and implements Distributed interface
  *
- * @author pron
+ * @author eitan
+ * @param <T>
  */
-public abstract class DistributedObject<T> implements CacheListener {
-    @Override
-    public abstract void invalidated(long id);
+public class DistributedObject<T> implements Distributed {
+    private final T obj;
+    private long id;
+
+    public DistributedObject(T obj) {
+        this.obj = obj;
+        this.id = 0;
+
+    }
+
+    public T getObj() {
+        return obj;
+    }
 
     @Override
-    public abstract void evicted(long id);
+    public void setId(long id) {
+        this.id = id;
+    }
 
     @Override
-    public abstract void received(long id, long version, ByteBuffer data);
+    public long getId() {
+        return id;
+    }
 
-    public abstract T get(Store store);
+    @Override
+    public int size() {
+        if (obj instanceof Persistable)
+            return ((Persistable) obj).size() + Long.SIZE / 8;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void write(ByteBuffer buffer) {
+        buffer.putLong(id);
+        if (obj instanceof Persistable) {
+            ((Persistable) obj).write(buffer);
+            return;
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void read(ByteBuffer buffer) {
+        id = buffer.getLong();
+        if (obj instanceof Persistable) {
+            ((Persistable) obj).read(buffer);
+            return;
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
