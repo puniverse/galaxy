a3a110adb9b8bb41668310e9f5eb242108c1f403
pron <ron.pressler@gmail.com>
2013-12-23 15:39:16 +0200

    DistributedReference

---
+++ src/main/java/co/paralleluniverse/galaxy/objects/DistributedReference.java
@@ -0,0 +1,133 @@
+/*
+ * Galaxy
+ * Copyright (C) 2012-2013 Parallel Universe Software Co.
+ * 
+ * This file is part of Galaxy.
+ *
+ * Galaxy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ *
+ * Galaxy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with Galaxy. If not, see <http://www.gnu.org/licenses/>.
+ */
+package co.paralleluniverse.galaxy.objects;
+
+import co.paralleluniverse.common.io.ByteBufferInputStream;
+import co.paralleluniverse.common.io.Persistable;
+import co.paralleluniverse.galaxy.CacheListener;
+import co.paralleluniverse.io.serialization.Serialization;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * Wraps T and implements Distributed interface
+ *
+ * @author eitan
+ * @param <T>
+ */
+public class DistributedReference<T> implements CacheListener, Persistable {
+    private volatile T obj;
+    private final long id;
+    private byte[] tmpBuffer;
+
+    protected DistributedReference(long id, T obj) {
+        this.obj = obj;
+        this.id = 0;
+    }
+
+    public T get() {
+        return obj;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    protected void clear() {
+        this.obj = null;
+    }
+
+    @Override
+    public void invalidated(long id) {
+    }
+
+    @Override
+    public void evicted(long id) {
+        clear();
+    }
+
+    @Override
+    public void received(long id, long version, ByteBuffer data) {
+        read(data);
+    }
+
+    /**
+     * This method is not thread safe!
+     *
+     * @return
+     */
+    @Override
+    public int size() {
+        if (obj instanceof Persistable)
+            return ((Persistable) obj).size();
+        else
+            return obj != null ? getSerialized().length : 0;
+    }
+
+    /**
+     * This method is not thread safe!
+     */
+    @Override
+    public void write(ByteBuffer buffer) {
+        if (obj instanceof Persistable)
+            ((Persistable) obj).write(buffer);
+        else {
+            if (obj != null)
+                buffer.put(getSerialized());
+            tmpBuffer = null;
+        }
+    }
+
+    byte[] getSerialized() {
+        if (tmpBuffer == null)
+            tmpBuffer = serialize(obj);
+        return tmpBuffer;
+    }
+
+    @Override
+    public void read(ByteBuffer buffer) {
+        if (obj instanceof Persistable)
+            ((Persistable) obj).read(buffer);
+        else
+            this.obj = deserialize(new ByteBufferInputStream(buffer));
+    }
+    
+    protected void set(T obj) {
+        this.obj = obj;
+    }
+
+    protected byte[] serialize(T obj) {
+        return obj != null ? Serialization.getInstance().write(obj) : null;
+    }
+
+    protected T deserialize(InputStream is) {
+        try {
+            return (T) Serialization.getInstance().read(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "[" + obj.getClass().getName() + "@" + System.identityHashCode(obj) + "]";
+    }
+}
---
+++ src/main/java/co/paralleluniverse/galaxy/objects/DistributedReferenceStore.java
@@ -0,0 +1,57 @@
+/*
+ * Galaxy
+ * Copyright (C) 2012-2013 Parallel Universe Software Co.
+ * 
+ * This file is part of Galaxy.
+ *
+ * Galaxy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ *
+ * Galaxy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with Galaxy. If not, see <http://www.gnu.org/licenses/>.
+ */
+package co.paralleluniverse.galaxy.objects;
+
+import co.paralleluniverse.galaxy.CacheListener;
+import co.paralleluniverse.galaxy.Store;
+
+/**
+ * Wraps T and implements Distributed interface
+ *
+ * @author eitan
+ * @param <T>
+ */
+public class DistributedReferenceStore<R extends DistributedReference<T>, T> {
+    private final Store store;
+
+    public DistributedReferenceStore(Store store) {
+        this.store = store;
+    }
+
+    public R getOrCreateRef(long lineId, Object arg) {
+        if (lineId <= 0)
+            return null;
+        CacheListener ref = store.getListener(lineId);
+        return (R) (ref != null ? ref : store.setListenerIfAbsent(lineId, createRef(lineId, null, arg)));
+    }
+
+    public R newRef(long lineId, T obj, Object arg) {
+        if (lineId <= 0)
+            return null;
+        assert store.getListener(lineId) == null;
+        R ref = createRef(lineId, obj, arg);
+        store.setListener(lineId, ref);
+        return ref;
+    }
+
+    protected R createRef(long id, T obj, Object arg) {
+        return (R)new DistributedReference<T>(id, obj);
+    }
+}
